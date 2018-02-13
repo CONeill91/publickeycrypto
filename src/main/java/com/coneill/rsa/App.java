a6a18e9a0bc7e45497e01bf72c6cd5eff7f50b4e
Conor O'Neill <conor.oneill91@gmail.com>
2018-02-13 23:31:02 +0000

    Adds xgcd

--- src/main/java/com/coneill/rsa/App.java
+++ src/main/java/com/coneill/rsa/App.java
@@ -33,8 +33,41 @@ public class App {
      * @return
      */
 
-    private static BigInteger xgcd(BigInteger a, BigInteger b){
-        return null; // todo
+    private static BigInteger[] xgcd(BigInteger a, BigInteger b) {
+        BigInteger x = a;
+        BigInteger y = b;
+        BigInteger x0 = BigInteger.ONE;
+        BigInteger x1 = BigInteger.ZERO;
+        BigInteger y0 = BigInteger.ZERO;
+        BigInteger y1 = BigInteger.ONE;
+        BigInteger[] qrem;
+        BigInteger[] result = new BigInteger[3]; // todo : I"m only interested in one of these aX
+
+        while (true) {
+            qrem = x.divideAndRemainder(y);
+            x = qrem[1];
+            x0 = x0.subtract(y0.multiply(qrem[0]));
+            x1 = x1.subtract(y1.multiply(qrem[0]));
+
+            if (x.equals(BigInteger.ZERO)) {
+                result[0]=y;
+                result[1]=y0;
+                result[2]=y1;
+                return result;
+            }
+
+            qrem = y.divideAndRemainder(x);
+            y = qrem[1];
+            y0 = y0.subtract(x0.multiply(qrem[0]));
+            y1 = y1.subtract(x1.multiply(qrem[0]));
+
+            if (y.equals(BigInteger.ZERO)) {
+                result[0]=x;
+                result[1]=x0;
+                result[2]=x1;
+                return result;
+            }
+        }
     }
 
     /**
